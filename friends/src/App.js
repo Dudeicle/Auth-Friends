2d4d45f53b895afaf1ac652ae783384a21d78951
Dudeicle <brian-griffiths@lambdastudents.com>
2020-07-21 17:46:05 -0400

    close to MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import LoginForm from './components/LoginForm.js';
+import FriendForm from './components/FriendsForm.js';
+import PrivateRoute from './components/PrivateRoute.js';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-    </div>
+    <Router>
+      <div className="App">
+
+        <div>
+          <Link to="/login">Login</Link>
+        </div>
+        <div>
+          <Link to="/protected">Protected Page</Link>
+        </div>
+        
+        <Switch>
+          <PrivateRoute exact path="/protected" component={FriendForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route component={LoginForm} />
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
--- friends/src/components/LoginForm.js
+++ friends/src/components/LoginForm.js
@@ -1,25 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function LoginForm() {
-    return (
-        <div>
-            <h1>User Login</h1>
-            <form>
-                <label>Username: 
-                    <input 
-                        name="login"
-                        type="text"
-                    />
-                </label>
-                <label>Password: 
-                    <input 
-                        name="login"
-                        type="text"
-                    />
-                </label>
-            </form>
-        </div>
-    )
+
+class LoginForm extends React.Component {
+
+    state = {
+        credentials: {
+            username: "",
+            password: ""
+        }
+    };
+
+    handleChanges = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = e => {
+        e.preventDefault();
+        // make a post request to the login page
+        axios
+            .post("http://localhost:5000/api/login", this.state.credentials)
+            .then(res => {
+                console.log(res);
+                localStorage.setItem("token", res.data.payload)
+                this.props.history.push("/protected")
+            })
+            .catch(err => console.log({ err }))
+    };
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>User Login</h1>
+                <form onSubmit={this.login}>
+                    <label>Username -:-
+                        <input 
+                            type="text"
+                            name="username"
+                            value={this.state.credentials.username}
+                            onChange={this.handleChanges}
+                        />
+                    </label>
+                    <br></br>
+                    <label>Password -:-
+                        <input 
+                            type="text"
+                            name="password"
+                            value={this.state.credentials.password}
+                            onChange={this.handleChanges}
+                        />
+                    </label>
+                    <br></br>
+                    <button>Login</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default LoginForm
